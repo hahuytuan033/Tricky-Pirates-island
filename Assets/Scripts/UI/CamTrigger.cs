1b7f21ab8bc10c9049afce6c86e4e5bd7d6a94ff
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-08 04:10:32 +0700

    Camera Transition

--- Assets/Scripts/UI/CamTrigger.cs
+++ Assets/Scripts/UI/CamTrigger.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class CamTrigger : MonoBehaviour
 {
+    public Vector3 newCamPos, newPlayerPos;
+
+    CamControls camControl;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        camControl = Camera.main.GetComponent<CamControls>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag=="Player")
+        {
+            camControl.minPos += newCamPos;
+            camControl.maxPos += newCamPos;
+            
+            collision.transform.position += newPlayerPos;
+        }
     }
+
 }
