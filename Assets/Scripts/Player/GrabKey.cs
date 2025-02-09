a276cf5bde182c795ab51ba377da9f03f1692b7d
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-09 17:42:12 +0700

    player grab key

---
+++ Assets/Scripts/Player/GrabKey.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrabKey : MonoBehaviour
+{
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform rayPoint;
+    [SerializeField] private float rayDistance;
+    private GameObject grabKey;
+    private int layerIndex;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        layerIndex = LayerMask.NameToLayer("Key");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, rayPoint.right, rayDistance);
+
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            if (grabKey == null && hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
+            {
+                // Grab key
+                grabKey = hitInfo.collider.gameObject;
+                grabKey.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabKey.transform.position = grabPoint.position;
+                grabKey.transform.SetParent(transform);
+            }
+            else if (grabKey != null)
+            {
+                // Release key
+                grabKey.GetComponent<Rigidbody2D>().isKinematic = false;
+                grabKey.transform.SetParent(null);
+                grabKey = null;
+            }
+        }
+    }
+}
