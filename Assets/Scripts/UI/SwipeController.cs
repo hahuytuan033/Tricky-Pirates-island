4a836d774d282d5ae3b4132cc0b35e67b8afd7a1
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-12 13:05:33 +0700

    Swipe level

---
+++ Assets/Scripts/UI/SwipeController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class SwipeController : MonoBehaviour
+{
+    [SerializeField] int maxPage;
+    int currentPage;
+    Vector3 targetPos;
+    [SerializeField] Vector3 pageStep;
+    [SerializeField] RectTransform levelPagesRect;
+    [SerializeField] float tweenTime;
+    [SerializeField] LeanTweenType tweenType;
+
+    private void Awake()
+    {
+        currentPage = 1;
+        targetPos = levelPagesRect.localPosition;
+    }
+
+    public void Next()
+    {
+        if (currentPage < maxPage)
+        {
+            currentPage++;
+            targetPos += pageStep;
+            MovePage();
+        }
+    }
+
+    public void Previous()
+    {
+        if (currentPage > 1)
+        {
+            currentPage--;
+            targetPos -= pageStep;
+            MovePage();
+        }
+    }
+
+    void MovePage()
+    {
+        levelPagesRect.LeanMoveLocal(targetPos, tweenTime).setEase(tweenType);
+    }
+}
