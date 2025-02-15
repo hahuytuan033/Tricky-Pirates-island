d289813f0922b2c5c817b734a671ec75191bca09
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-15 14:45:40 +0700

    using effect UI with Dotween

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,16 +2,23 @@ using System.Numerics;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
-    public RectTransform Menu;
+    [SerializeField] private RectTransform pausePanelRect;
+    [SerializeField] float topPosY, middlePosY;
+    [SerializeField] float tweenDuration;
+    [SerializeField] CanvasGroup canvasGroup;
+
 
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        PausePanelIntro();
     }
 
     public void Exit()
@@ -20,8 +27,9 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1;
     }
 
-    public void Resume()
+    public async void Resume()
     {
+       await PausePanelOutro();
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
@@ -30,5 +38,18 @@ public class PauseMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+    }
+
+    void PausePanelIntro()
+    {
+        canvasGroup.DOFade(1, tweenDuration).SetUpdate(true);
+        pausePanelRect.DOAnchorPosY(middlePosY, tweenDuration).SetUpdate(true);
+        
+    }
+
+    async Task PausePanelOutro()
+    {
+        canvasGroup.DOFade(0, tweenDuration).SetUpdate(true);
+        await pausePanelRect.DOAnchorPosY(topPosY, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
     }
 }
