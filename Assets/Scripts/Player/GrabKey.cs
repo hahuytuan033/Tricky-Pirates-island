44a8cca9c2eaff98f26e5d5881fc31c366fffe59
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-17 01:45:00 +0700

    fix grab key

--- Assets/Scripts/Player/GrabKey.cs
+++ Assets/Scripts/Player/GrabKey.cs
@@ -19,37 +19,54 @@ public class GrabKey : MonoBehaviour
 
     // Update is called once per frame
     [System.Obsolete]
-    void Update()
+    // void Update()
+    // {
+    //     RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, rayPoint.right, rayDistance);
+
+    //     if (Keyboard.current.eKey.wasPressedThisFrame)
+    //     {
+    //         if (grabKey == null && hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
+    //         {
+    //             // Grab key
+    //             grabKey = hitInfo.collider.gameObject;
+    //             grabKey.GetComponent<Rigidbody2D>().isKinematic = true;
+    //             grabKey.transform.rotation = Quaternion.Euler(180, 0, 90);
+    //             grabKey.transform.position = grabPoint.position;
+    //             grabKey.transform.SetParent(transform);
+
+    //             // Get the animator component
+    //             keyAnimator = grabKey.GetComponent<Animator>();
+    //             keyAnimator.SetBool("isGrabbing", true);
+
+    //         }
+    //         else if (grabKey != null)
+    //         {
+    //             // Release key
+    //             grabKey.GetComponent<Rigidbody2D>().isKinematic = false;
+    //             grabKey.transform.rotation = Quaternion.Euler(0, 0, 0);
+    //             grabKey.transform.SetParent(null);
+
+    //             //Set animation to idle
+    //             keyAnimator.SetBool("isGrabbing", false);
+    //             grabKey = null;
+    //         }
+    //     }
+    // }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, rayPoint.right, rayDistance);
-
-        if (Keyboard.current.eKey.wasPressedThisFrame)
+        if (grabKey == null && collision.gameObject.layer == layerIndex)
         {
-            if (grabKey == null && hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
-            {
-                // Grab key
-                grabKey = hitInfo.collider.gameObject;
-                grabKey.GetComponent<Rigidbody2D>().isKinematic = true;
-                grabKey.transform.rotation = Quaternion.Euler(180, 0, 90);
-                grabKey.transform.position = grabPoint.position;
-                grabKey.transform.SetParent(transform);
+            // Nhặt chìa khóa
+            grabKey = collision.gameObject;
+            grabKey.GetComponent<Rigidbody2D>().isKinematic = true;
+            grabKey.transform.rotation = Quaternion.Euler(180, 0, 90);
+            grabKey.transform.position = grabPoint.position;
+            grabKey.transform.SetParent(transform);
 
-                // Get the animator component
-                keyAnimator = grabKey.GetComponent<Animator>();
-                keyAnimator.SetBool("isGrabbing", true);
-
-            }
-            else if (grabKey != null)
-            {
-                // Release key
-                grabKey.GetComponent<Rigidbody2D>().isKinematic = false;
-                grabKey.transform.rotation = Quaternion.Euler(0, 0, 0);
-                grabKey.transform.SetParent(null);
-
-                //Set animation to idle
-                keyAnimator.SetBool("isGrabbing", false);
-                grabKey = null;
-            }
+            // Lấy Animator component
+            keyAnimator = grabKey.GetComponent<Animator>();
+            keyAnimator.SetBool("isGrabbing", true);
         }
     }
 }
