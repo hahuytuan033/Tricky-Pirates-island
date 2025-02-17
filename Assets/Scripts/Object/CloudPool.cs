200961197c8bdbadd6e74be30a71a87a451ce2a6
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-02-17 08:18:41 +0700

    fix scenes

--- Assets/Scripts/Object/CloudPool.cs
+++ Assets/Scripts/Object/CloudPool.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public class CloudPool : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
