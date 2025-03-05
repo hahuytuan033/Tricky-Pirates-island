5916d8d2deb639260689e0a1b81ff004052c37ba
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-03-05 23:46:25 +0700

    save file level 3

--- Assets/Scripts/Level/Level_3.cs
+++ Assets/Scripts/Level/Level_3.cs
@@ -1,13 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level_3 : MonoBehaviour
 {
-   public GameObject objectToHide;
-    public GameObject objectToShow;
+   public Button yesButton;        
+    public Button noButton;         
+    public GameObject panel1;       
+    public GameObject volume;    
+    public GameObject prefabToShow; // Prefab sẽ hiện lên khi nhấn Yes
 
-    public void OnButtonClick()
+    private void Start()
     {
-        objectToHide.SetActive(false);
-        objectToShow.SetActive(true);
+        // Thêm sự kiện click cho các nút
+        yesButton.onClick.AddListener(OnYesButtonClick);
+        noButton.onClick.AddListener(OnNoButtonClick);
+
+        // // Đảm bảo trạng thái ban đầu
+        // panel1.SetActive(true);
+        // panel2.SetActive(false);
+        // if (prefabToShow != null)
+        // {
+        //     prefabToShow.SetActive(false);
+        // }
+    }
+
+    private void OnYesButtonClick()
+    {
+        // Ẩn panel 1
+        panel1.SetActive(false);
+        
+        // Hiện panel 2
+        volume.SetActive(true);
+        
+        // Hiện prefab (chỉ khi nhấn Yes)
+        if (prefabToShow != null)
+        {
+            prefabToShow.SetActive(true);
+        }
+    }
+
+    private void OnNoButtonClick()
+    {
+        // Ẩn panel 1
+        panel1.SetActive(false);
+        
+        // Hiện panel 2
+        volume.SetActive(true);
+        // Không hiện prefab khi nhấn No
     }
 }
