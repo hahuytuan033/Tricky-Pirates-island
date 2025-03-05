6eee1831dfb0b1fa0a79711c586c47f177a63c70
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-03-05 17:57:47 +0700

    save volume setting

--- Assets/Scripts/UI/Musicbutton.cs
+++ Assets/Scripts/UI/Musicbutton.cs
@@ -1,7 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class Musicbutton : MonoBehaviour
 {
-    
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        if(PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+ 
+    }
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetMusicVolume();
+        SetSFXVolume();
+    }
 }
